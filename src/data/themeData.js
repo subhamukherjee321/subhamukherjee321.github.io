ca73b1d0295ac98d7a0cca886f82084bb1b96855
subhamukherjee321 <subhamukherjee321@gmail.com>
2022-12-12 19:39:33 +0530

    changed color

--- src/data/themeData.js
+++ src/data/themeData.js
@@ -1,13 +1,29 @@
 /* eslint-disable */
-import { pinkThemegirl } from '../theme/images'
-import { 
-    greenThemeLight, greenThemeDark, bwThemeLight, bwThemeDark, blueThemeLight, blueThemeDark, redThemeLight, redThemeDark, orangeThemeLight, orangeThemeDark, purpleThemeLight, purpleThemeDark, pinkThemeLight, pinkThemeDark, yellowThemeLight, yellowThemeDark
-} from '../theme/theme'
+// import { useState } from "react";
+import { pinkThemegirl } from "../theme/images";
+import {
+  greenThemeLight,
+  greenThemeDark,
+  bwThemeLight,
+  bwThemeDark,
+  blueThemeLight,
+  blueThemeDark,
+  redThemeLight,
+  redThemeDark,
+  orangeThemeLight,
+  orangeThemeDark,
+  purpleThemeLight,
+  purpleThemeDark,
+  pinkThemeLight,
+  pinkThemeDark,
+  yellowThemeLight,
+  yellowThemeDark,
+} from "../theme/theme";
 
+// const [changeTheme, setChangeTheme] = useState(false);
 
 export const themeData = {
-    theme:redThemeLight
-}
-
+  theme: purpleThemeDark
+};
 
 // Choose theme from above
